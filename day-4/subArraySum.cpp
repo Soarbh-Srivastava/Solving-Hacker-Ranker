28c7a53a37b068c4c64fd68710911c927ab96c4d
Soarbh-Srivastava <22bcs11518@cuchd.in>
2023-12-24 21:42:02 +0530

    correct solution

--- day-4/subArraySum.cpp
+++ day-4/subArraySum.cpp
@@ -5,24 +5,32 @@ class Solution
     //Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int>arr, int n, long long s)
     {
-        // Your code here
-      long long sum = arr[0];
-
-        if (sum == s) return {1, 1};
-
-        int l = 0, r = 0;
-
-        while (r < n - 1) {
-            if ((sum += arr[++r]) <= s) {
-                if (sum == s) return {l + 1, r + 1};
-            } else if (l < r) {
-                sum -= arr[l++];
-            } else {
-                sum = arr[++l];
-                r = l;
+         // Your code here
+            long long sum = arr[0];
+        
+        if(sum == s) return {1,1};
+        
+        int l=0,r=0;
+        while(r<n-1){
+            if(sum+arr[r+1]<=s){
+                r++;
+                sum+=arr[r];
             }
+            else{
+                if(l==r){
+                    l++;
+                    r++;
+                    sum=arr[l];
+                }
+                else{
+                    sum-=arr[l];
+                    l++;
+                   }
+                }
+                if(sum==s){
+                    return{l+1,r+1};
+                }
+            }
+            return{-1};
         }
-
-        return {-1};
-    }
 };
